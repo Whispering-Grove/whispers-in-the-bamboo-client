9a7271e02abebb10a0c70db0ced18f4b49e86189
hyu-dev <hyu630115@gmail.com>
2025-04-28 23:40:55 +0900

    :art: 경로 변경

--- src/app/providers/router/config.tsx
+++ src/app/providers/router/config.tsx
@@ -1,8 +1,8 @@
 import { RouteObject } from 'react-router-dom'
 import { MainPage } from '@pages/MainPage'
 import { LoginPage } from '@pages/LoginPage'
-import { LoginGuard } from '@processes/auth/loginGuard.tsx:'
 import { Paths } from '@shared/config/Paths.ts'
+import { LoginGuard } from '@widgets/auth/loginGuard.tsx'
 
 export const routerConfig: RouteObject[] = [
   {
