59d3341f7e60d2f56d4b8138cd7d25bf69e42f0f
hyu-dev <hyu630115@gmail.com>
2025-04-28 22:19:29 +0900

    :package: 프로젝트 설정 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react()],
-  resolve: {
-    alias: [
-      { find: '@', replacement: '/src' },
-      { find: '@apis', replacement: '/src/apis' },
-      { find: '@assets', replacement: '/src/assets' },
-    ],
-  },
+  plugins: [react(), tsconfigPaths()],
 })
