8e700585fc126d081d5fbdab0b72954f7de635a9
hyu-dev <hyu630115@gmail.com>
2025-05-13 21:46:33 +0900

    Fix: 경로 오류 수정

--- src/app/providers/router/config.tsx
+++ src/app/providers/router/config.tsx
@@ -1,8 +1,8 @@
 import { RouteObject } from 'react-router-dom'
 import { Main } from '@pages/Main'
 import { Login } from '@pages/Login'
-import { Paths } from '@shared/config/paths.ts'
 import { LoginGuard } from '@widgets/auth/loginGuard.tsx'
+import { Paths } from '@shared/config/paths.ts'
 
 export const routerConfig: RouteObject[] = [
   {
