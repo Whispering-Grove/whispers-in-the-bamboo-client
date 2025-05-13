02104baa2b30d5b622da0f1269ccbc348d76e09c
hyu-dev <hyu630115@gmail.com>
2025-05-14 00:56:12 +0900

    Chore: 로그인 사이즈 변경

--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -5,10 +5,7 @@ export const Container = styled.div`
   position: relative;
   width: 100%;
   height: 100%;
-  min-width: 700px;
-  max-width: 1280px;
-  min-height: 393px;
-  max-height: 720px;
+  min-width: 1300px;
   background-image: url(${titleBackground});
   background-repeat: no-repeat;
   background-position: top;
